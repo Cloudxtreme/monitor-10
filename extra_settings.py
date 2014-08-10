c7916eb41259993b635c4246d4d4b6d13c0e0efd
Guojing Tang <guojingtang.1988@gmail.com>
2014-08-10 13:42:38 +0800

    Remove secret from repo

--- extra_settings.py
+++ extra_settings.py
@@ -1,9 +1,2 @@
 HEAD_CAMPAIGN_SETTINGS = ("Chanel Preloved Handbags", "Chanel Vintage this week on Brandsfever Luxury", "https://luxury-brandsfever.s3.amazonaws.com/top-campaign-banner/Prada%20LANDING1.jpg")
 
-# FACEBOOK_APP_ID = FACEBOOK_API_KEY = '729808580363676'
-# FACEBOOK_SECRET_KEY = 'fc60b148d159a8d5f82ca09d2e4d5bad'
-
-# PAYPAL_CLIENT_ID = 'ATNViBCMkBs3l7TF3cnySFQLM6bqbxxzEs6QYV9shBwDQy2GozGAVSJnbZbF'
-# PAYPAL_SECRET = 'EIe8zhD1PIRD29M4HSHHi9jQ_WyFDimrfGqizQYaURjxlqa31NPYnE6wuU5b'
-# PAYPAL_REDIRECT_URI = 'http://127.0.0.1:8888/login/paypal/'
-# PAYPAL_ENDPOINT_LOGIN = 'https://www.paypal.com'
